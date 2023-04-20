b13aa54d98d05f99ec971928046695dd7ccdb769
Anton Kostin <nitka5710@gmail.com>
2023-04-20 11:39:17 +0300

    first commit

---
+++ RunGroupWebApp/Models/Adress.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RunGroupWebApp.Models
+{
+    public class Adress
+    {
+        [Key]
+        public string Id { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+    }
+}
